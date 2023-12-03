5bf9a031fe8617ee05e4e889c2ad6a8a2200b827
Jelena Stanković <151223989+CecaJeca11@users.noreply.github.com>
2023-12-03 22:12:35 +0100

    Add files via upload

---
+++ Assigment1.java
@@ -0,0 +1,9 @@
+public class Assigment1 {
+    public static void main(String[] args) {
+                double r = 10;
+                double H = 50;
+                double PI = 3.14;
+                double V = r*r*PI*H;
+                System.out.println(V);
+    }
+}
